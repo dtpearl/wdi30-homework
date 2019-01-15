e77283dba28a353596d4f978d85e4e36f9b90131
David Pearl <davidtpearl@gmail.com>
2019-01-15 17:02:13 +1100

    Set up mountains DB, added empty views pages.

---
+++ david_pearl/week05/mountain_app/app/controllers/mountains_controller.rb
@@ -0,0 +1,3 @@
+class MountainsController < ApplicationController
+
+end
---
+++ david_pearl/week05/mountain_app/db/create_mountains.sql
@@ -0,0 +1,8 @@
+CREATE TABLE mountains (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  country TEXT,
+  height FLOAT,
+  mountain_range TEXT,
+  image TEXT
+);
