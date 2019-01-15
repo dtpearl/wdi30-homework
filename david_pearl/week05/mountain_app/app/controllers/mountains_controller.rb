4341c12dcd1d4d1a1758b3d3c34eb7b06e3869b4
David Pearl <davidtpearl@gmail.com>
2019-01-16 00:03:35 +1100

    Mountains DB - Homework 15/01/2019

--- david_pearl/week05/mountain_app/app/controllers/mountains_controller.rb
+++ david_pearl/week05/mountain_app/app/controllers/mountains_controller.rb
@@ -1,3 +1,47 @@
 class MountainsController < ApplicationController
 
+  def index
+    @mountains = Mountain.all
+    # Implicit return - renders index.html.erb
+  end
+
+  def show
+    @mountain = Mountain.find params[:id]
+  end
+
+  def new
+  end
+
+  def create
+    mountain = Mountain.new
+    mountain.name = params[:name]
+    mountain.country = params[:country]
+    mountain.height = params[:height]
+    mountain.mountain_range = params[:mountain_range]
+    mountain.image = params[:image]
+    mountain.save
+
+    redirect_to mountain_path(mountain.id)
+  end
+
+  def edit
+    @mountain = Mountain.find params[:id]
+  end
+
+  def update
+    mountain = Mountain.find params[:id]
+    mountain.name = params[:name]
+    mountain.country = params[:country]
+    mountain.height = params[:height]
+    mountain.mountain_range = params[:mountain_range]
+    mountain.image = params[:image]
+    mountain.save # UPDATE DB record with the new information.
+
+    redirect_to mountain_path(mountain.id)
+  end
+
+  def destroy
+    # Deletes a mountain from the DB
+    
+  end
 end
--- david_pearl/week05/mountain_app/config/routes.rb
+++ david_pearl/week05/mountain_app/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # localhost:3000/rails/info/routes
+  root :to => 'mountains#index'
+
+  get '/mountains' => 'mountains#index'
+  get '/mountains/new' => 'mountains#new'
+  post '/mountains' => 'mountains#create'
+  get '/mountains/:id' => 'mountains#show', :as => 'mountain'
+  get '/mountains/:id/edit' => 'mountains#edit', :as => 'mountain_edit'
+  post '/mountains/:id' => 'mountains#update'
+  delete '/mountains/:id' => 'mountains#destroy'
 end
